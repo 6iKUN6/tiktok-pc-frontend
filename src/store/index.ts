4d91ffee216c3e44d0022a78165ae6b0fa33b352
Sparkle <2705263765@qq.com>
2025-01-14 20:00:18 +0800

    feat: 🚀 修复状态管理导致的内存泄漏，新增friend

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,5 @@
+export * from './modules/baseStore';
 export * from './modules/testStore';
 export * from './modules/sider';
 export * from './modules/videoContainer';
+export * from './modules/videoStore';
